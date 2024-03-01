2902e36c25c12d1f237ba60583f23b8e98f1b730
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-03-01 12:55:23 +0000

    Will this break?

--- Greet.java
+++ Greet.java
@@ -1,8 +1,9 @@
 // Greet.java
 
 public class Greet {
-    public static void main(String[] args){
-        System.out.println("hello");
-        System.out.println("How are you?");
+    public static void main(String[] args) {
+        String greeting = "Good morning";  // ADDED
+        System.out.println(greeting);  // MODIFIED
+        System.out.println("How are you?"); 
     }
 }
